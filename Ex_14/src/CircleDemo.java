7a5301422150cc7cd42fd781ae6548bc0c8d1708
mdtha <mdtha@ImThariq>
2021-06-06 19:30:02 +0530

    master

--- Ex_14/src/CircleDemo.java
+++ Ex_14/src/CircleDemo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CircleDemo {
+    class CircleDemo {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		 System.out.print("Enter the radius: ");
